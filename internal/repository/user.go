b7706b9517780492acfec4167612f29ebb712e7b
朱卓霖 <zhulin5421@126.com>
2024-09-13 21:48:06 +0800

    <fix> :  新增通过name寻找用户

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -12,16 +12,18 @@ type UserRepository interface {
 	Update(ctx context.Context, user *models.User) error
 	FindByID(ctx context.Context, id uint) (*models.User, error)
 	AllUsers(ctx context.Context) ([]*models.User, error)
+	FindByUsername(ctx context.Context, username string) (*models.User, error)
+	UpdateByUsername(ctx context.Context, username string, user *models.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) UserRepository {
 	return &userRepository{
 		db: db,
 	}
-}
-
-type userRepository struct {
-	db *gorm.DB
 }
 
 func (r *userRepository) Create(ctx context.Context, user *models.User) error {
@@ -59,4 +61,16 @@ func (r *userRepository) AllUsers(ctx context.Context) ([]*models.User, error) {
 		return nil, err
 	}
 	return users, nil
+}
+
+func (r *userRepository) FindByUsername(ctx context.Context, username string) (*models.User, error) {
+	var user models.User
+	if err := r.db.WithContext(ctx).Where("username = ?", username).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) UpdateByUsername(ctx context.Context, username string, updatedUser *models.User) error {
+	return r.db.WithContext(ctx).Model(&models.User{}).Where("username = ?", username).Updates(updatedUser).Error
 }
