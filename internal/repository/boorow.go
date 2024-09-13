fe662d84dc8dabcd5145e473e0684aefab36e391
朱卓霖 <zhulin5421@126.com>
2024-09-13 22:55:43 +0800

    <feat> :新增通过用户名查询图书的定义

--- internal/repository/boorow.go
+++ internal/repository/boorow.go
@@ -12,6 +12,7 @@ type BorrowRepository interface {
 	FindByID(ctx context.Context, id uint) (*models.Borrow, error)
 	AllBorrowRecords(ctx context.Context) ([]*models.Borrow, error)
 	FindByUserID(ctx context.Context, userID uint) ([]*models.Borrow, error)
+	FindByUsername(ctx context.Context, username string) ([]*models.Borrow, error)
 }
 
 func NewBorrowRepository(db *gorm.DB) BorrowRepository {
@@ -57,6 +58,13 @@ func (r *borrowRepository) AllBorrowRecords(ctx context.Context) ([]*models.Borr
 func (r *borrowRepository) FindByUserID(ctx context.Context, userID uint) ([]*models.Borrow, error) {
 	var records []*models.Borrow
 	if err := r.db.WithContext(ctx).Where("user_id = ?", userID).Find(&records).Error; err != nil {
+		return nil, err
+	}
+	return records, nil
+}
+func (r *borrowRepository) FindByUsername(ctx context.Context, username string) ([]*models.Borrow, error) {
+	var records []*models.Borrow
+	if err := r.db.WithContext(ctx).Joins("JOIN users ON users.id = borrows.user_id").Where("users.username = ?", username).Find(&records).Error; err != nil {
 		return nil, err
 	}
 	return records, nil
